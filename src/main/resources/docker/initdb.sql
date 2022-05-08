66e0c3b2d736abd08904ba8acfe77a4a91885628
Szfeduś <krystianszwedo@wp.pl>
2022-05-08 13:14:55 +0200

    added docker-compose.yml and aplication properties

--- src/main/resources/docker/initdb.sql
+++ src/main/resources/docker/initdb.sql
@@ -19,12 +19,12 @@ CREATE TABLE IF NOT EXISTS Reservations(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id    uuid not null,
     prelection_id uuid not null,
-    constraint fk_tehnician
+    constraint fk_user
         foreign key(user_id)
             references Users(id),
-    constraint fk_tehnician
+    constraint fk_prelection
         foreign key(prelection_id)
-            references Prelection(id),
+            references Prelection(id)
     );
 
 INSERT INTO Users(login,email)
@@ -32,23 +32,32 @@ values ('S7f3dz14g1','krystianszwedo@wp.pl');
 INSERT INTO Users(login,email)
 values ('antek','ant@wp.pl');
 
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('10.00 - 11-45','First','Java od zera.');
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('12.00 - 13-45','First','Co to jest Hiberante');
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('14.00 - 15-45','First','Podstawy springa');
 
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('10.00 - 11-45','Second','C# od zera.');
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('12.00 - 13-45','Second','C vs C++');
-INSERT INTO Users(times,subject,title)
-values ('14.00 - 15-45','Second','');
+INSERT INTO Prelection(times,subject,title)
+values ('14.00 - 15-45','Second','Piszemy czysty kod');
 
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('10.00 - 11-45','Third','Python od zera.');
-INSERT INTO Users(times,subject,title)
+INSERT INTO Prelection(times,subject,title)
 values ('12.00 - 13-45','Third','Tworzymy projekt MVC');
-INSERT INTO Users(times,subject,title)
-values ('14.00 - 15-45','Third','Co to jest django');
+INSERT INTO Prelection(times,subject,title)
+values ('14.00 - 15-45','Third','Co to jest django?');
+
+INSERT INTO Reservations(user_id,prelection_id)
+values ((SELECT id FROM Users where login = 'antek'),
+    (SELECT id FROM Prelection where title = 'Co to jest django?'));
+
+INSERT INTO Reservations(user_id,prelection_id)
+values ((SELECT id FROM Users where login = 'S7f3dz14g1'),
+    (SELECT id FROM Prelection where title = 'Python od zera.'));
+
